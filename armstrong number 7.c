c8cda413c6b288deea832403f3d3bc6d3f55d4b2
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-02-07 16:43:21 +0530

    Create armstrong number 7.c

---
+++ armstrong number 7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>]
+int main()
+{
+ int 371,n,r,res= 0;
+printf("Enter a three digit num\n");
+scanf("%d", &n);
+371=num;
+while(371!=0)
+r=371;
+r*r*r;
+371/=10;
+if(res=n)
+printf("%d is an Armstrong number",n);
+else
+printf("%d is not an Armstrong number",n);
+getch();
+return 0;
+}
