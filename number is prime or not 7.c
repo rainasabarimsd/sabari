1c330c391ac63faea9f196759260a69a06a7f196
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-02-07 16:10:47 +0530

    Create number is prime or not 7.c

---
+++ number is prime or not 7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a;
+ clrscr();
+ printf("Enter the number");
+ scanf("%d",&a);
+ if(a%a=0 && a%a=1)
+ {
+  printf("yes");
+  }
+  else
+  printf("no");
+  getch();
+  return 0;
+  }
