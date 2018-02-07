4444fb8df146800ef1d989f93a94df558202658f
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-02-07 16:36:24 +0530

    Create prime numbers between two numbers 7.c

---
+++ prime numbers between two numbers 7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int i,j,b,n;
+printf("Enter the first number");
+scanf("%d",&n);
+printf("Enter the last number");
+scanf("%d",&b);
+for(i=n;i<b;i++)
+if(i%j==0)
+{
+for(j=2;j<+i;i++)
+{
+c=1;
+break;
+}
+if(c==0)
+{
+printf("the prime number is &d",c);
+}
+getch();
+return 0;
+}
