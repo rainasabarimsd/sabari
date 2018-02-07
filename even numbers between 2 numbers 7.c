955adfc3629a2ab52464bfd5644c8471acaace00
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-02-07 16:28:43 +0530

    Create even numbers between 2 numbers 7.c

---
+++ even numbers between 2 numbers 7.c
@@ -0,0 +1,17 @@
+#include<stduio.h>
+#include<conio.h>
+int main()
+{
+int a,i;
+printf("Enter all the even numbers");
+scanf("%d",&a)
+printf("The even numbers are");
+i=0;
+while(i<n)
+{
+if(i%2)
+}
+printf("All the even numbeers are:");
+getch();
+return 0;
+}
