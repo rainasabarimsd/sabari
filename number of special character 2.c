4c7e540463bfa8f775aaa596225331f227b88cce
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-03-02 13:46:17 +0530

    Create number of special character 2.c

---
+++ number of special character 2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ char a[1000];
+	int n,i,count=0;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]>='0'&&a[i]<='9')
+	    {
+	        count++;
+	    }
+	    else
+	    {
+	        continue;
+	    }
+	}
+	printf("%d",count);
+	return 0;
+}
