6095212b4474a21b470518cd98d5f0ec73c4ad09
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-03-05 16:06:32 +0530

    Create string s and a number 5.c

---
+++ string s and a number 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+ int a,i;
+char b[10];
+for(i=0;i<=7;i++)
+{
+scanf("%c",&b[i]);
+}
+scanf("%d",&a);
+for(i=0;i<7;i++)
+{
+printf("%s",b);
+getch();
+return 0;
+}
