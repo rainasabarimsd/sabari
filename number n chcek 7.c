bb3efbb377691ec2bd2b71a802d02b13080aa61c
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-03-07 15:55:40 +0530

    Create number n chcek 7.c

---
+++ number n chcek 7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+ int i,a;
+printf("enter the value");
+scanf("%d",&a);
+for(i=1;i<=10;i++)
+{
+printf("%d",i);
+}
+if(a==i);
+{
+printf("yes");
+else
+{
+printf("no");
+}
+getch();
+}
