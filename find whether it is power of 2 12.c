8e08a3526ad7349d7b1a241ccb417ecf2c0f8448
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-03-12 16:02:34 +0530

    Create find whether it is power of 2 12.c

---
+++ find whether it is power of 2 12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf(" %d is power of 2",n);
+}
+else
+{
+printf("not a power of two");
+}
+}
