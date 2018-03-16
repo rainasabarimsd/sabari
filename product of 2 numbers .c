929ea9a6b1e772fb64d707a65fad8ec5e422a4e7
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-03-16 03:20:18 -0700

    Create product of 2 numbers .c

---
+++ product of 2 numbers .c
@@ -0,0 +1,18 @@
+#incluclude<stdio.h>
+#include<conio.h>
+int main()
+{
+  int n,m,res;
+    scanf("%d",&n);
+    scanf("%d",&m);
+    res=n*m;
+    if(res%2==0)
+    {
+        printf("%d is even",res);
+    }
+    else
+    {
+        printf("%d is odd",res);
+    }
+    return 0;
+    }
