bfbcd66a3528fa7aecd8c48572e222a11b450143
rainasabarimsd <36067313+rainasabarimsd@users.noreply.github.com>
2018-03-19 16:27:32 +0530

    Create print the 1st character .c

---
+++ print the 1st character .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char s1[50];
+    int i,c=0;
+    printf("enter the string :");
+    scanf("%[^n]s",s1);
+    for(i=0;s1[i]!='\0';i++){
+if(s1[i]==' ')
+        {
+            c++;
+        }
+
+}
+ printf("no of words are :%d",c);
+}
